f4976efb51770bde59b4535c7623c00e4a3b7435
joearrigo <joeyma917@gmail.com>
2024-02-19 12:04:32 -0500

    Minor update to serial code and its structuring

--- Control/Serial.py
+++ Control/Serial.py
@@ -1,19 +1,19 @@
 import serial
 import serial.tools.list_ports
 from threading import Thread
-import socket
-import time
-import os
 
-def listen(ser):
+rbSer = None
+connectd = True
+
+def serial_listen(ser):
     while True:
         line = ser.readline()
         if line.decode() != "" and line.decode() != "\n":
             pline = line.decode().replace("\n", "")
             print(pline)
 
-rbSer = None
-connectd = True
+def serial_send(ser, message):
+    ser.write(message.encode())
 
 #COM PORT STUFF
 ports = list(serial.tools.list_ports.comports())
@@ -21,7 +21,7 @@ for p in ports:
     rbSer = serial.Serial(p[0], 9600, timeout=10)
     line = rbSer.readline()
     print(line.decode())
-    rbSer.write("<sp2414>\n".encode())
+    serial_send(rbSer, "<sp2414>\n")
     line = rbSer.readline()
     if "<sp2414>" in line.decode():
         print(line.decode())
@@ -30,8 +30,10 @@ for p in ports:
         break
 
 #MAIN BODY OF OPERATION
+#TODO: Move this thread to main control body code when it is created.
+#TODO: Ideally, we'll be able to use the functions in this file for something like a cpp-style include 
 
-thread = Thread(target=listen, args=(rbSer))
+thread = Thread(target=serial_listen, args=(rbSer))
 thread.start()
 
 while connectd == True:
