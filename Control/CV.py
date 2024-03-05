02b6b53d571868820f65e97b823bedffe27dcb58
joearrigo <joeyma917@gmail.com>
2024-03-04 22:15:01 -0500

    Move to RPI

--- Control/CV.py
+++ Control/CV.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2 as cv
+from matplotlib import pyplot as plt
+import matplotlib as mpl
 
 pixWidthTop  = 1034
 pixHeightMin = 560
@@ -13,32 +15,45 @@ if not cap.isOpened():
     print("Cannot open camera")
     exit()
 
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
-    
-width  = cap.get(3)  # float `width`
-height = cap.get(4)  # float `height`
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 
-print("Width is " + str(width) + " height is " + str(height))
+print("Width is " + str(cap.get(3)) + " height is " + str(cap.get(4)))
+    
+cap2 = cv.VideoCapture("/dev/video0")
+if not cap2.isOpened():
+    print("Cannot open camera 2")
+    exit()
+    
+cap2.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+cap2.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+
+print("Width2 is " + str(cap2.get(3)) + " height2 is " + str(cap2.get(4)))
     
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    ret2, frame2 = cap2.read()
+    
+    frame_new=cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame2_new=cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
+    
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     
+    if not ret2:
+        print("Can't receive frame2 (stream end?). Exiting ...")
+        break
     
-    # Our operations on the frame come here
-    if not (pixHeightMin == -1):
-        cv.line(frame, (imgX, imgY), (imgX, pixHeightMin),(0,0,255), 5)
-        cv.line(frame, (imgX, pixHeightMin), ((int(imgX/2)+int(pixWidthTop/2)), 0),(0,0,255), 5)
-        cv.line(frame, (0, imgY), (0, pixHeightMin),(0,0,255), 5)
-        cv.line(frame, (0, pixHeightMin), ((int(imgX/2)-int(pixWidthTop/2)), 0),(0,0,255), 5)
+    stereo = cv.StereoBM.create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(frame_new,frame2_new)
 
     # Display the resulting frame
-    cv.imshow('frame', frame)
+    # cv.imshow('frame', disparity)
+    cv.imshow('frame',disparity)
+    
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
