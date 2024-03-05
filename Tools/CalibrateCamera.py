e85af33034a6ca86c8a455715c050bddc9b66c23
joearrigo <joeyma917@gmail.com>
2024-03-05 13:25:54 -0500

    Added Calibrations and TOols

---
+++ Tools/CalibrateCamera.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2 as cv
+import glob
+import pickle
+
+###################
+###  PROPERTIES ###
+###################
+
+resolutionX = 1920
+resolutionY = 1080
+cameraName = "LogiNorm"
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('*.jpg')
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+w = 1920
+h = 1080
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+#dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+
+toSerialize = {"mapx": mapx, "mapy": mapy}
+with open(cameraName+".remap", "wb") as outfile:
+    pickle.dump(toSerialize, outfile)
