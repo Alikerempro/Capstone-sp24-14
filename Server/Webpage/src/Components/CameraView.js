35601af0d2f1116e82592f9e968f970f6b9ff4f2
joearrigo <joeyma917@gmail.com>
2024-03-05 00:10:31 -0500

    Attempting to stream webcam from server

--- Server/Webpage/src/Components/CameraView.js
+++ Server/Webpage/src/Components/CameraView.js
@@ -3,7 +3,9 @@ import View from "./View"
 export default function CameraView(){
     return (<>
     <View dragable={true}>
-        <div id="cameraStream"/>
+        <div id="cameraStream">
+            <img src="{{ url_for('camStream')}}"/>
+        </div>
         <button id="enableStream"/>
         <button id="enableCV"/>
         <button id="savePicture"/>
