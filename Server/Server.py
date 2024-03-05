85ed79578785217d1ec1b1b0b26bc8e52f538dd5
joearrigo <joeyma917@gmail.com>
2024-03-05 15:26:59 -0500

    Pi updating

--- Server/Server.py
+++ Server/Server.py
@@ -1,37 +1,58 @@
 from flask import Flask, render_template, Response
 import cv2 as cv
+import pickle 
 
 app = Flask(__name__, static_url_path='',
                   static_folder='Webpage/dist',
                   template_folder='Webpage/dist')
 
-cameras = [cv.VideoCapture('/dev/video0'), cv.VideoCapture("/dev/video2")]
+camera1 = cv.VideoCapture('/dev/video0')
+camera2 = cv.VideoCapture("/dev/video3")
 
-def gen_frames(plain):  # generate frame by frame from camera
+camera1.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+camera1.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+
+camera2.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+camera2.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+
+#Video 0 is Norm
+#Video 2 is Carl
+
+
+videoMap0 = {}
+videoMap1 = {}
+
+with open("LogiCarl.remap", "rb") as infile:
+    videoMap0 = pickle.load(infile)
+
+with open("LogiNorm.remap", "rb") as infile:
+    videoMap1 = pickle.load(infile)
+
+
+def gen_frames():  # generate frame by frame from camera
     while True:
         # Capture frame-by-frame
-        success, frame = cameras[0].read()  # read the camera frame
-	success2, frame2 = cameras[1].read()
-        if not success or not success2:
+        success, frame = camera2.read()  # read the camera frame
+        if not success:
             break
-        else:
-	    if plain == True:
-		   ret, buffer = cv.imencode('.jpg', frame)
-		   frame = buffer.tobytes()
-                   yield (b'--frame\r\n'
-                         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-	    else:
-		   
-		   ret, buffer = cv.imencode('.jpg', frame)
-                   frame = buffer.tobytes()
-                   yield (b'--frame\r\n'
-                         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        success, frame2 = camera1.read()
+        if not success:
+            break
+        ret, buffer = cv.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-
-@app.route('/video_feed')
-def video_feed():
+@app.route('/video_feed_reg')
+def video_feed_reg():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+#@app.route('/video_feed_CV')
+#def video_feed_CV():
+#    return Response(gen_frames(False), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
@@ -39,4 +60,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(use_reloader=False, port=5000, threaded=True, host='0.0.0.0')
+    app.run(debug=False, use_reloader=False, port=5000, threaded=True, host='0.0.0.0')
